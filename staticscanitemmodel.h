7d208438c1ad0c709ff7de8e4f66e6b0518ecedd
horsicq <horsicq@gmail.com>
2020-07-25 00:04:07 +0200

    Fix

--- staticscanitemmodel.h
+++ staticscanitemmodel.h
@@ -42,8 +42,8 @@ public:
     QVariant headerData(int nSection,Qt::Orientation orientation,int nRole=Qt::DisplayRole) const override;
     QModelIndex index(int row,int column,const QModelIndex &pParent=QModelIndex()) const override;
     QModelIndex parent(const QModelIndex &index) const override;
-    int rowCount(const QModelIndex &pParent=QModelIndex()) const override;
-    int columnCount(const QModelIndex &pParent=QModelIndex()) const override;
+    int rowCount(const QModelIndex &parent=QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent=QModelIndex()) const override;
     QVariant data(const QModelIndex &index,int role=Qt::DisplayRole) const override;
     Qt::ItemFlags flags(const QModelIndex &index) const override;
     QString toXML();
