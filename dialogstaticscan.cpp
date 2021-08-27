a0980b6c527dcbe16a00e0d8a83ababdf9335d60
horsicq <horsicq@gmail.com>
2021-08-28 00:47:15 +0200

    Fix: 2021-08-28

--- dialogstaticscan.cpp
+++ dialogstaticscan.cpp
@@ -69,6 +69,6 @@ void DialogStaticScan::scan()
     ds.setData(g_pDevice,&options,&scanResult);
     ds.exec();
 
-    QString sSaveFileName=XBinary::getResultFileName(g_pDevice,"result.txt");
+    QString sSaveFileName=XBinary::getResultFileName(g_pDevice,QString("%1.txt").arg(tr("Result")));
     ui->widgetResult->setData(scanResult,sSaveFileName);
 }
