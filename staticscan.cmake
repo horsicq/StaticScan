9db6b7829053bd15096f6ef98fe9641d087d7b7a
horsicq <horsicq@gmail.com>
2021-10-27 19:52:49 +0200

    Fix: 2021-10-27

--- staticscan.cmake
+++ staticscan.cmake
@@ -1,8 +1,9 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-# TODO
+include(${CMAKE_CURRENT_LIST_DIR}/../SpecAbstract/specabstract.cmake)
 
 set(STATICSCAN_SOURCES
+    ${SPECABSTRACT_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/staticscan.cpp
     ${CMAKE_CURRENT_LIST_DIR}/staticscanitem.cpp
     ${CMAKE_CURRENT_LIST_DIR}/staticscanitemmodel.cpp
