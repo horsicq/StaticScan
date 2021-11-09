ea17d4bb07e44a6c4a881945a175d86fb08ff664
horsicq <horsicq@gmail.com>
2021-11-09 19:10:02 +0100

    Fix: 2021-11-09

--- formresult.cmake
+++ formresult.cmake
@@ -5,7 +5,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/dialogstaticscanprocess.cmake)
 set(FORMRESULT_SOURCES
     ${DIALOGSTATICSCANPROCESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogstaticscan.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/formresult.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogstaticscan.ui
+    ${CMAKE_CURRENT_LIST_DIR}/dialogstaticscandirectory.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogstaticscandirectory.ui
+    ${CMAKE_CURRENT_LIST_DIR}/formresult.cpp
     ${CMAKE_CURRENT_LIST_DIR}/formresult.ui
 )
