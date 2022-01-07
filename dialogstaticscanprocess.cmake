a4368e5ac970edc18d41354500c2db8762a74cbd
horsicq <horsicq@gmail.com>
2022-01-08 00:27:15 +0100

    Fix: 2022-01-08

--- dialogstaticscanprocess.cmake
+++ dialogstaticscanprocess.cmake
@@ -1,9 +1,11 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/staticscan.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XOptions/xoptionswidget.cmake)
 
 set(DIALOGSTATICSCANPROCESS_SOURCES
     ${STATICSCAN_SOURCES}
+    ${XOPTIONSWIDGET_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogstaticscanprocess.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogstaticscanprocess.ui
 )
