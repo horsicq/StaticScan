9af0da79f25d56a8bf58b09e146f85b792b9e25d
horsicq <horsicq@gmail.com>
2022-03-23 09:03:14 +0100

    Fix: 2022-03-23

--- staticscan.cmake
+++ staticscan.cmake
@@ -4,7 +4,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/../SpecAbstract/specabstract.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../Formats/scanitem.cmake)
 
 set(STATICSCAN_SOURCES
-    ${SPECABSTRACT_SOURCES}
     ${SCANITEM_SOURCES}
+    ${SPECABSTRACT_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/staticscan.cpp
 )
